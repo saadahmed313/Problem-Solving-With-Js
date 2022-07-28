0b8a331411e3bcfe4fb29f393886f7cc03e9eedc
saadahmed313 <89286569+saadahmed313@users.noreply.github.com>
2022-07-28 06:52:03 +0200

    Add files via upload

---
+++ Remove Character From String/main.js
@@ -0,0 +1,9 @@
+// function remove_char_from(word, c){
+  
+//     return word.split("").filter((e) => e !== c.toUpperCase() && e !== c.toLowerCase()  ).join("")
+// };
+
+// Testing Ouput
+console.log(remove_char_from("ElddzeroD WebDD ddSchool", "d")); // Elzero Web School
+console.log(remove_char_from("ElxzeroX Web Sxchool", "x")); // Elzero Web School
+console.log(remove_char_from("Elzero@ Web@@ @@School", "@")); // Elzero Web School
