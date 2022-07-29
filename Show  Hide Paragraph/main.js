19f45c641408a1abb137997d2961db83f4742082
saadahmed313 <89286569+saadahmed313@users.noreply.github.com>
2022-07-29 02:51:45 +0200

    Add files via upload

---
+++ Show  Hide Paragraph/main.js
@@ -0,0 +1,13 @@
+let btn = document.querySelector("button");
+let p =document.querySelector("p");
+let re = window.getComputedStyle(p);
+let c = 2;
+btn.onclick=function(){
+if(c%2===0){
+ p.style.display ="none";
+ c++;
+}else{
+    p.style.display ="block";
+c++
+}
+}
